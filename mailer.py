185d67d469a21103da2cb0dd640bf71b2a979fd0
ericdmann <eric@ericmann.net>
2013-07-19 12:11:26 -0700

    Create mailer.py

---
+++ mailer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+
+gmail_user = "YOUREMAIL"
+gmail_pwd = "YOURPASSWORD"
+
+def mail(to, subject, text, attach):
+   msg = MIMEMultipart()
+
+   msg['From'] = gmail_user
+   msg['To'] = to
+   msg['Subject'] = subject
+
+   
+   mailServer = smtplib.SMTP("smtp.gmail.com", 587)
+   mailServer.ehlo()
+   mailServer.starttls()
+   mailServer.ehlo()
+   mailServer.login(gmail_user, gmail_pwd)
+   mailServer.sendmail(gmail_user, to, msg.as_string())
+   # Should be mailServer.quit(), but that crashes...
+   mailServer.close()
+
+mail("eric.mann@lposd.org",
+   "Your device is running away!",
+   "Run Forrest, Run!",
+   "my_picture.jpg")
